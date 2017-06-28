73b0899201eec88136a77127963c1c66874ea1e6
ekanna <koti@ekanna.com>
2017-06-28 14:35:16 +0530

    fork of https://github.com/bugst/go-serial

---
+++ syscall_openbsd.go
@@ -0,0 +1,9 @@
+//
+// Copyright 2014-2017 Cristian Maglie. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+package serial // import "github.com/ekanna/serialk"
+
+//sys ioctl(fd int, req uint64, data uintptr) (err error)
